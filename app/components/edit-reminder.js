1e820afe350e52175f6fbbaf3239306e0740f10e
Justin Wood <jwood11atx@gmail.com>
2017-02-14 15:56:12 -0700

    Cherry pick fbcb350. Remove reset functionality

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -24,11 +24,6 @@ export default Ember.Component.extend({
         record.save();
       })
     },
-    reset(){
-      this.element.children[0].textContent = this.model.data.title;
-      this.element.children[1].textContent = this.model.data.date;
-      this.element.children[2].textContent = this.model.data.notes;
-    },
     update(field, i){
       this.set(field, this.element.children[i].textContent)
     },
