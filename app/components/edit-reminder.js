55f7932444d911e7d2b940b0fbee153d5fe4c318
Justin Wood <jwood11atx@gmail.com>
2017-02-14 12:24:56 -0700

    Cherry pick a97ead7

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -1,18 +1,42 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+
+  title: "",
+  date: "",
+  notes: "",
+
   actions: {
-    edit(){
+    save(){
+      this.get("store").findRecord("reminder", this.model.id)
+      .then((record) => {
+        var date = new Date(this.get("date").split("/"))
+
+        this.get("title") ? record.set("title", this.get("title")) :
+        record.set("title", this.model.data.title)
+
+        this.get("date") ? record.set("date", date) :
+        record.set("date", this.model.data.date)
+
+        this.get("notes") ? record.set("notes", this.get("notes")) :
+        record.set("notes", this.model.data.notes)
+
+        record.save();
+      })
+    },
+    reset(){
       this.get("store").findRecord("reminder", this.model.id)
       .then(function(record){
-        var date = new Date(
-          document.querySelector(".spec-edit-date").innerHTML.split("/"));
-        record.set("title",
-          document.querySelector(".spec-edit-title").innerHTML);
-        record.set("date", date);
-        record.set("notes",
-          document.querySelector(".spec-edit-notes").innerHTML);
+        document.querySelector(".spec-edit-title")
+          .innerHTML = record.data.title;
+        document.querySelector(".spec-edit-date")
+          .innerHTML = record.data.date;
+        document.querySelector(".spec-edit-notes")
+          .innerHTML = record.data.notes;
       })
-    }
+    },
+    update(data, field, i){
+      this.set(field, data.element.children[i].textContent)
+    },
   }
 });
